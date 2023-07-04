ff499ddab0f42495fa7483c1ca2c546ff37bfac4
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-07-04 16:41:11 -0500

    work on melas logic

---
+++ src/middlewares/meals.middleware.js
@@ -0,0 +1,33 @@
+const AppError = require('../utils/appError');
+const Local = require('../models/restaurants.model');
+const catchAsync = require('../utils/catchAsync');
+const Meals = require('../models/meals.model');
+
+exports.validMeal = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+
+  const meal = await Meals.findOne({
+    where: {
+      id,
+      status: 'active',
+    },
+    attributes: {
+      exclude: ['createdAt', 'updatedAt', 'restaurantId', 'status'],
+    },
+    include: [
+      {
+        model: Local,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'status'],
+        },
+      },
+    ],
+  });
+
+  if (!meal) {
+    return next(new AppError(`The meal with id:${id} it's not found `, 404));
+  }
+
+  req.meal = meal;
+  next();
+});
